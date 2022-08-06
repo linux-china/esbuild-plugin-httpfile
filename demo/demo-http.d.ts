481381d073390d32b2f04a15a67ce11c5b26db66
linux_china <libing.chen@gmail.com>
2022-08-06 13:36:50 -0700

    chore: rename

--- demo/demo-http.d.ts
+++ demo/demo-http.d.ts
@@ -1,4 +1,4 @@
-declare module "*.http" {
+declare module "*demo.http" {
     export function myIp(): Promise<Response>;
 
     export function postTest(params: { nick: string, host: string, uuid: string }): Promise<Response>;
