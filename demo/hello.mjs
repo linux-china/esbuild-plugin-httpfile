56dfa7896ed838b987c1423b6e11563bb3b7708a
linux_china <libing.chen@gmail.com>
2022-08-06 13:17:30 -0700

    chore: code format

--- demo/hello.mjs
+++ demo/hello.mjs
@@ -7,7 +7,7 @@ console.log(await response.json());
 
 // simple http post
 console.log("==============================================================");
-response = await postTest({nick: "test", host:"httpbin.org","uuid":"c8389930-1071-4b88-9676-30b9ba7f2343"});
+response = await postTest({nick: "test", host: "httpbin.org", "uuid": "c8389930-1071-4b88-9676-30b9ba7f2343"});
 console.log(await response.json());
 
 //simple graphql request
@@ -17,5 +17,5 @@ console.log(await response.json());
 
 //graphql request with variables
 console.log("==============================================================");
-response = await graphqlDemo({id:2});
+response = await graphqlDemo({id: 2});
 console.log(await response.json());
