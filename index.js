c21419c5189a0a89b2b80c5bba766ec98585e416
linux_china <libing.chen@gmail.com>
2022-08-06 14:26:59 -0700

    feat: add loggingVerbose to output generated code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const {resolve} = require('path');
+const {resolve, basename} = require('path');
 const fs = require('fs');
 const {parseHttpfile} = require('./httpfile')
 
 const name = 'httpfile'
+let loggingVerbose = false;
 
 const setup = ({onResolve, onLoad}) => {
     onResolve({filter: /\.(http)$/}, ({path, resolveDir}) => ({
@@ -18,10 +19,39 @@ const buildHttpfileFunctions = async ({path, pluginData}) => {
     const originalPath = resolve(resolveDir, path);
     const httpfileText = await fs.promises.readFile(originalPath, {encoding: 'utf8'});
     const targets = parseHttpfile(httpfileText);
+    // generate javascript stub code
     let contents = targets.map(target => {
         return target.toCode();
     }).join("\n\n");
+    if (loggingVerbose) {
+        // generate typescript declaration file
+        let declareFileName = basename(originalPath);
+        let declaredApiList = targets.map(target => {
+            return target.toApiDeclare();
+        }).join("\n    ");
+        let moduleDeclareCode = `declare module '*${declareFileName}' {\n    ${declaredApiList}\n}`;
+        // logging
+        let declaredFileName = declareFileName.replace(".http", "-http.d.ts");
+        console.log("=====================" + declaredFileName + "==========================================");
+        console.log(moduleDeclareCode);
+        console.log("=====================" + declareFileName + ".js========================================");
+        console.log(contents);
+        console.log("=======================================================================================");
+
+    }
     return {contents};
 }
 
-module.exports = {name, setup};
+/**
+ * build esbuild httpfile plugin
+ * @param {boolean=} verbose - enable verbose logging
+ * @returns {{name: string, setup: function}} esbuild plugin object
+ */
+function buildHttpFilePlugin(verbose) {
+    if (verbose) {
+        loggingVerbose = true;
+    }
+    return {name, setup};
+}
+
+module.exports = buildHttpFilePlugin
