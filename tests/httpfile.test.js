9fae8072a5c09ccacfb39cac9eb37d881b6f0ee7
linux_china <libing.chen@gmail.com>
2022-08-07 10:26:50 -0700

    chore: add process.env.NODE_ENV = 'production';

--- tests/httpfile.test.js
+++ tests/httpfile.test.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import {parseHttpfile} from '../httpfile.js';
 
 function testParse() {
+    // process.env.NODE_ENV = 'production'; // uncomment this line to test production mode
     const httpfile_text = fs.readFileSync('demo/demo.http', 'utf8');
     let targets = parseHttpfile(httpfile_text);
     for (let target of targets) {
